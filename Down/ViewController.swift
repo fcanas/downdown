ea11b1a190c39e0842f0e259e6ff6bc2362bb9a9
Fabian Canas <fcanas@gmail.com>
2016-01-26 22:14:16 -0500

    Add html previewing to test app

--- Down/ViewController.swift
+++ Down/ViewController.swift
@@ -8,22 +8,40 @@
 
 import Cocoa
 import SwiftDown
+import WebKit
 
 class ViewController: NSViewController, NSTextViewDelegate {
 
     @IBOutlet var editableText :NSTextView!
     @IBOutlet var outputView: NSTextView!
+    @IBOutlet weak var webView: WebView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // Do any additional setup after loading the view.
+        editableText.string = "# Title\n\n" +
+        "## Subtitle\n\n" +
+        "* Things\n" +
+        "* Objects\n" +
+        "* Items\n\n" +
+        "*This* is a paragraph that\n" +
+        "can be spread across *multiple*\n" +
+        "lines  in the source file, but will\n" +
+        "get consolidated into a single\n" +
+        "paragraph. It can `also` contain\n" +
+        "[links](http://www.fabiancanas.com).\n\n" +
+        "```\n" +
+        "code does not get processed and *can* \n" +
+        "exhibit markdown stuff without processing.\n" +
+        "```"
     }
 
     func textDidChange(notification: NSNotification) {
         if let textView = notification.object as? NSTextView {
-            
-            outputView.string = markdown(textView.string!)
+            let html = markdown(textView.string!)
+            outputView.string = html
+            webView.mainFrame.loadHTMLString(html, baseURL: NSURL(string:"/"))
         }
     }
     
